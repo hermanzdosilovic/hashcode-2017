60cb97dacd5ecee33642b57a5fedf1bab41a69b3
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2017-02-23 21:15:40 +0100

    Latency and validation

--- qualification/greedy.py
+++ qualification/greedy.py
@@ -1,11 +1,11 @@
 V, E, R, C, X = tuple(int(i) for i in input().split(" "))
 
-video_size = tuple(int(i) for i in input().split(" ")) # 1 x V
+video_size = tuple(int(i) for i in input().split(" "))
 
-endpoint_to_data_center_latency = [] # 1 x E
-endpoint_to_cache_server_latency = [[0 for i in range(C)] for j in range(E)] # E x C
-endpoint_cache_server_connection = [[] for i in range(E)] # 1 x E
-cache_server_endpoint_connection = [[] for i in range(C)] # 1 x C
+endpoint_to_data_center_latency = []
+endpoint_to_cache_server_latency = [[0 for i in range(C)] for j in range(E)]
+endpoint_cache_server_connection = [[] for i in range(E)]
+cache_server_endpoint_connection = [[] for i in range(C)]
 for e in range(E):
     Ld, K = tuple(int(i) for i in input().split(" "))
     endpoint_to_data_center_latency.append(Ld)
@@ -20,3 +20,19 @@ for r in range(R):
     Rv, Re, Rn = tuple(int(i) for i in input().split(" "))
     endpoint_video_request[Re].append((Rv, Rn))
 
+def calculate_total_latency(cache_server_videos_arrangement):
+    total_latency = 0
+    for endpoint_id, endpoint_requests in enumerate(endpoint_video_request):
+        for video_id, number_of_requests in endpoint_requests:
+            min_latency = endpoint_to_data_center_latency[endpoint_id]
+            for cache_server_id in endpoint_cache_server_connection[endpoint_id]:
+                if video_id in cache_server_videos_arrangement[cache_server_id]:
+                    min_latency = min(min_latency, endpoint_to_cache_server_latency[endpoint_id, cache_server_id])
+            total_latency += min_latency*number_of_requests
+    return total_latency
+
+def is_arrangement_valid(cache_server_videos_arrangement):
+    for video_ids in cache_server_videos_arrangement:
+        if sum([video_size[video_id] for video_id in video_ids]) > X:
+            return False
+    return True
