6bbe03c1a9cfe7af9d268747ac715296354aeb44
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2017-02-24 11:53:33 +0100

    Minimizing total latency

--- qualification/greedy.py
+++ qualification/greedy.py
@@ -26,12 +26,18 @@ for endpoint_id in range(E):
     endpoint_video_request_queue[endpoint_id] = [(x, i) for i, x in enumerate(endpoint_video_request[endpoint_id]) if x]
     endpoint_video_request_queue[endpoint_id].sort(reverse=True)
 
+best_latency = max(endpoint_to_data_center_latency)*total_number_of_requests
 number_of_iterations = int(sys.argv[2])
-best_score = 0
 while number_of_iterations > 0:
     number_of_iterations -= 1
     cache_server_video_arrangement = [[False for i in range(V)] for j in range(C)]
     cache_server_availability = [X for i in range(C)]
+    endpoint_video_latency = [[0 for i in range(V)] for j in range(E)]
+    current_latency = 0
+    for i in range(E):
+        for j in range(V):
+            endpoint_video_latency[i][j] = endpoint_to_data_center_latency[i]*endpoint_video_request[i][j]
+            current_latency += endpoint_video_latency[i][j]
 
     for endpoint_id, video_requests in enumerate(endpoint_video_request_queue):
         for number_of_requests, video_id in video_requests:
@@ -42,35 +48,34 @@ while number_of_iterations > 0:
                     continue
                 cache_server_availability[cache_server_id] -= video_size[video_id]
                 cache_server_video_arrangement[cache_server_id][video_id] = True
+                
+                for dependent_endpoint_id in range(E):
+                    latency = endpoint_to_cache_server_latency[dependent_endpoint_id][cache_server_id]
+                    if not latency:
+                        continue
+                    if not endpoint_video_request[dependent_endpoint_id][video_id]:
+                        continue
+                    current_latency -= endpoint_video_latency[dependent_endpoint_id][video_id]
+                    endpoint_video_latency[dependent_endpoint_id][video_id] = min(
+                        endpoint_video_latency[dependent_endpoint_id][video_id],
+                        latency*endpoint_video_request[dependent_endpoint_id][video_id]
+                    )
+                    current_latency += endpoint_video_latency[dependent_endpoint_id][video_id]
                 break
         random.shuffle(endpoint_video_request_queue[endpoint_id])
-    
-    current_latency_save = 0
-    for endpoint_id, video_requests in enumerate(endpoint_video_request_queue):
-        for number_of_requests, video_id in video_requests:
-            min_latency = endpoint_to_data_center_latency[endpoint_id]
-            for cache_server_id in range(C):
-                if not endpoint_to_cache_server_latency[endpoint_id][cache_server_id]:
-                    continue
-                elif not cache_server_video_arrangement[cache_server_id][video_id]:
-                    continue
-                min_latency = min(
-                    min_latency,
-                    endpoint_to_cache_server_latency[endpoint_id][cache_server_id]
-                )
-            current_latency_save += (endpoint_to_data_center_latency[endpoint_id] - min_latency)*number_of_requests
-
-    current_score = round(current_latency_save/total_number_of_requests*1000) 
-    if current_score > best_score:
-        best_score = current_score
-        print("New best score:", best_score, file=sys.stderr)
+                
+    if current_latency < best_latency:
+        best_latency = current_latency
+        print("New best latency:", best_latency, file=sys.stderr)
         
         f = open(sys.argv[1], "w")
         number_of_cache_servers = C - cache_server_availability.count(X)
         print(number_of_cache_servers, file=f)
         for cache_server_id in range(C):
-            if cache_server_availability[cache_server_id] == X:
-                continue
-            print(cache_server_id, *[i for i in range(V) if cache_server_video_arrangement[cache_server_id][i]], file=f)
+            print(cache_server_id, end=" ", file=f)
+            for video_id in range(V):
+                if cache_server_video_arrangement[cache_server_id][video_id]:
+                    print(video_id, end=" ", file=f)
+            print(file=f)
         f.close()
-        print("Finding new best score ...")
+        print("Finding new best latency ...")
